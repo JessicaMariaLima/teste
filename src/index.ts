19ac7f5f6577e1e2ebd8c3fc8eba25a8d16bca1b
JESSICA MARIA DE LIMA <jessica.mlima20@senacsp.edu.br>
2023-11-08 15:01:01 -0300

    teste

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import validator from 'validator'
+
+console.log(validator.isEmail('jessica@gmail.com'))
+
+
+let nome: string = "João"
+let idade: number = 18
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos!"`)
