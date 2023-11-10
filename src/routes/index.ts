c4125389a10533d94dd66a9b82777e1e512590fb
JESSICA MARIA DE LIMA <jessica.mlima20@senacsp.edu.br>
2023-11-10 17:16:14 -0300

    mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,33 +1,84 @@
-import express, {Router,Request, Response} from 'express'
-import {Server} from 'http'
+import express, { Router, Request, Response } from 'express'
+import { request, Server } from 'http'
 
 const router = Router()
 
 //criando a primeira rota
-router.get("/",(req,res) => {
-    res.send("Olá Mundo!")
+// router.get("/",(req,res) => {
+//     res.send("Olá Mundo!")
+// })
+
+router.get("/", (req, res) => {
+
+    //    let user = {
+    //         name: 'José',
+    //         age: 28,
+    //         showWelcome : true
+    //    }
+
+
+    // res.render('home',{
+    //     name: 'João',
+    //     lastName:'Silva',
+    //     age: 20,
+    //     showWelcome : true
+    // })
+
+    let age: number = 20;
+    let showAge: boolean = false;
+
+
+    if (age >= 18) {
+        showAge = true
+    }
+    res.render('home', {
+        name: 'João',
+        lastName: 'Silva',
+        age: 20,
+        showAge,
+        produtos: [
+            {titulo: 'Produto x', Preco: 200},
+            {titulo: 'Produto y', Preco: 290},
+            {titulo: 'Produto Z', Preco: 450},
+
+        ]
+
+    })
+})
+// let user : string = "Jess Lima"
+
+// router.get('/contato', (req: Request, res: Response) =>{
+//     res.send("Esta é a página de contato")
+// })
+
+router.get('/sobre', (req: Request, res: Response) => {
+    res.render('about')
+
+})
+
+router.get('/contato', (req: Request, res: Response) => {
+    res.send("Contato = 11 9588-5222")
 })
 
 
-router.get('/contato', (req: Request, res: Response) =>{
-    res.send("Esta é a página de contato")
-})
+
+
+
 
 // Rota estática
 
-router.get('/noticia/titulo-da-noticia', (req: Request, res: Response) =>{
+router.get('/noticia/titulo-da-noticia', (req: Request, res: Response) => {
     res.send("Noticia Aparecendo na Tela")
 })
 
 //Rota dinâmica
-router.get('/noticia/:slug', (req:Request, res: Response) =>
-{
+router.get('/noticia/:slug', (req: Request, res: Response) => {
     let slug: string = req.params.slug
     res.send(`Noticia: ${slug}`)
 })
 
-router.get('/voo/:origem-:destino', (req:Request, res:Response) =>{
-    let {origem,destino} = req.params
+router.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+    let { origem, destino } = req.params
     res.send(`Procurando voos de ${origem} até ${destino}`)
 })
 // forma de acesso http://localhost:3000/voo/sp-rj
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 // importando a biblioteca
 import express, {Request, Response} from 'express'
-import {Server} from 'http'
+import {Server} from 'http';
+import path from 'path';
+import mustache from 'mustache-express';
 // importando nosso arquivo index
 import mainRoutes from './routes/index'
 
+
+
 //usando EXPRESS
 const server = express()
-
+ 
+//configurando mustache
+server.set('view engine','mustache')
+//configurando o caminho da pasta views
+server.set('views',path.join(__dirname,'views'))
+server.engine('mustache',mustache())
+ 
+//configurando o caminho da pasta public
+server.use(express.static(path.join(__dirname,'../public')))
+ 
 server.use(mainRoutes)
-
-
-//Gerando o servidor na porta 3000
-server.listen(3000)
-
+ 
+//criando a página não encontrada
+server.use((req:Request, res: Response) =>{
+    res.status(404).send('Página não encontrada')
+})
+ 
+//gerando o servidor na porta 3000
+server.listen(2000)
